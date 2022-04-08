95ec6e512e283580080844be14c241c6a6081ed3
mfoiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-04-08 17:47:14 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,3 +2,7 @@
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
 sh -c "echo THE CHECKOUT PATH IS: $GITHUB_WORKSPACE"
+sh -c "for FILE in ${GITHUB_WORKSPACE}/*.*
+do
+  echo $(basename ${FILE})
+done"
