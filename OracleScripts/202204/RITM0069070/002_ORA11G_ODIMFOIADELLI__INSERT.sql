6304eeaa07e6e8dc5c01458de0628097c217acd9
mfoiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-04-12 16:39:49 +0200

    Update and rename 002_DB__SCHEMA_DESC.sql to 002_ORA11G_ODIMFOIADELLI__INSERT.sql

---
+++ OracleScripts/202204/RITM0069070/002_ORA11G_ODIMFOIADELLI__INSERT.sql
@@ -0,0 +1,29 @@
+INSERT INTO utente_test_liquibase (
+    nome,
+    cognome,
+    email
+) VALUES (
+    "Matteo",
+    "Foiadelli",
+    "matteo.foiadelli@icteam.it"
+);
+INSERT INTO utente_test_liquibase (
+    nome,
+    cognome,
+    email
+) VALUES (
+    "Tiziano",
+    "Bezzi",
+    "tiziano.bezzi@icteam.it"
+);
+INSERT INTO utente_test_liquibase (
+    nome,
+    cognome,
+    email
+) VALUES (
+    "Simona",
+    "Carrara",
+    "simona.carrara@icteam.it"
+);
+
+COMMIT;
