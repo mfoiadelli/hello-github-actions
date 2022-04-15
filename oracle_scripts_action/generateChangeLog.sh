8a4f92d5763457674338f92f65b3b71b539583d8
mfoiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-04-15 11:46:10 +0200

    Update generateChangeLog.sh

--- oracle_scripts_action/generateChangeLog.sh
+++ oracle_scripts_action/generateChangeLog.sh
@@ -55,11 +55,11 @@ echo "${CHANGELOG_HEADER}" > ${changelogFile}
 
 # ENSURE TO SKIP THE FOR LOOP BODY IF NO MATCH IS FOUND (NO SQL FILES FOUND IN THE DIRECTORY)
 shopt -s nullglob extglob nocaseglob
-fileNameRegex="^[0-9]{3}_${databaseName}_.+\.(sql|plsql)$"
+fileNameRegex="[0-9]{3}_${databaseName}_.+\.sql$"
 for scriptFilePath in ./*.sql ./*.plsql
 do
      echo $(basename "${scriptFilePath}")
-     if [[ $(basename "${scriptFilePath}") =~ [0-9]{3}_${databaseName}_.+\.sql ]] 
+     if [[ $(basename "${scriptFilePath}") =~ $fileNameRegex ]] 
           then
                echo "${scriptFilePath} matches"
      fi
