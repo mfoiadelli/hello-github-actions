5bf6d61e4024ddb1cab3ed59f9afc392a156ad7a
mfoiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-04-15 11:42:18 +0200

    Update generateChangeLog.sh

--- oracle_scripts_action/generateChangeLog.sh
+++ oracle_scripts_action/generateChangeLog.sh
@@ -59,7 +59,7 @@ fileNameRegex="^[0-9]{3}_${databaseName}_.+\.(sql|plsql)$"
 for scriptFilePath in ./*.sql ./*.plsql
 do
      echo $(basename "${scriptFilePath}")
-     if [[ $(basename "${scriptFilePath}") =~ [0-9]{3}_ORA11G_.+\.sql ]] 
+     if [[ $(basename "${scriptFilePath}") =~ [0-9]{3}_${databaseName}_.+\.sql ]] 
           then
                echo "${scriptFilePath} matches"
      fi
