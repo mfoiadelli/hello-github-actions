8a20023602230f1f66f4802d03f532c259dbf367
mfoiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-04-21 13:37:28 +0200

    Update importOdiObjects.sh

--- odi_actions/import_objects/importOdiObjects.sh
+++ odi_actions/import_objects/importOdiObjects.sh
@@ -18,7 +18,7 @@ generateConnectionProperties() {
    	echo "schemaPwd=${odiSchemaPwd}" >> ${connectionPropertiesFile}
    	echo "workrep=${odiWorkRepositoryName}" >> ${connectionPropertiesFile}
    	echo "odiUser=${odiUsername}" >> ${connectionPropertiesFile}
-   	echo "odiUserPwd=${odiSchemaPwd}" >> ${connectionPropertiesFile}
+   	echo "odiUserPwd=${odiUserPwd}" >> ${connectionPropertiesFile}
 }
 
 validateInputs() {
@@ -55,6 +55,6 @@ echo -n "Executing import-objects shell..."
 result=$(/Users/matteofoiadelli/Documents/Development/OdiUtils/src/import-objects.sh -c ${connectionPropertiesFile} ${odiObjectsDirectory}; echo $?)
 [[ ${result} -eq 0 ]] && echo " Done!" || echo " Failed!"
 echo -n "Cleaning up..."
-#rm ${connectionPropertiesFile}
+rm ${connectionPropertiesFile}
 echo " Done!"
 exit $result
