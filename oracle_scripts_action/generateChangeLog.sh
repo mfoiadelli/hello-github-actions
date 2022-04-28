a0c738e0c3dcc0bcf46cef5e0bebcde73fa797e2
mfoiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-04-28 18:33:33 +0200

    Update generateChangeLog.sh

--- oracle_scripts_action/generateChangeLog.sh
+++ oracle_scripts_action/generateChangeLog.sh
@@ -30,7 +30,7 @@ populateChangeLog() {
   <databaseChangeLog
              xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-             xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.1.xsd">
+             xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.9.0.xsd">
   ' > "${1}"
 
   # ENSURE TO SKIP THE FOR LOOP BODY IF NO MATCH IS FOUND (NO SQL FILES FOUND IN THE DIRECTORY)
