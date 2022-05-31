68ec048642394bee854132643ad3fefde8ef109f
Matteo Foiadelli <98540641+mfoiadelli@users.noreply.github.com>
2022-05-31 16:58:16 +0200

    Update importOdiObjects.sh

--- odi_actions/import_objects/importOdiObjects.sh
+++ odi_actions/import_objects/importOdiObjects.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+. /home/oracle/.bash_profile
 
 getOdiObjectsDirectory() {
 	echo "Getting Objects Directory..."
 	cd "$GITHUB_WORKSPACE" || exit 1
-	odiObjectsDirectory=$(find . -regex "./ODI/[0-9][0-9][0-9][0-9][0-9][0-9]/${ritmName}/objects")
-	[[ ! $odiObjectsDirectory ]] && echo "::error::ERROR: Cannot find the directory corresponding to the provided RITM identifier (${ritmName})!" && exit 1
+	targetDir="encrypted_objects"
+	if [[ -z ${key} ]]
+	then
+		targetDir="objects"
+	fi
+	odiObjectsDirectory=$(find . -regex "./ODI/[0-9][0-9][0-9][0-9][0-9][0-9]/${ritmName}/${targetDir}")
+	[[ ! $odiObjectsDirectory ]] && echo "::error::ERROR: Cannot find the directory corresponding to the provided RITM identifier (${ritmName}/${targetDir})!" && exit 1
 }
 
 generateConnectionProperties() {
@@ -26,7 +32,7 @@ validateInputs() {
 	[[ -z ${odiSchemaPwd} ]] && echo "::error::ERROR: ODI Schema password cannot be null. Please provide a valid password for the ODI schema." && exit 1
 	[[ -z ${odiWorkRepositoryName} ]] && echo "::error::ERROR: The ODI work repository cannot be null. Please provide a valid ODI work repository name to work with." && exit 1
 	[[ -z ${odiUsername} ]] && echo "::error::ERROR: ODI username cannot be null. Please provide a valid username in order to establish a connection to the ODI instance." && exit 1
-	[[ -z ${odiSchemaPwd} ]] && echo "::error::ERROR: ODI user password cannot be null. Please provide a valid user password in order to establish a connection to the ODI instance." && exit 1
+	[[ -z ${odiUserPwd} ]] && echo "::error::ERROR: ODI user password cannot be null. Please provide a valid user password in order to establish a connection to the ODI instance." && exit 1
 }
 
 # Get the shell inputs
@@ -37,13 +43,19 @@ odiSchemaPwd=$4
 odiWorkRepositoryName=$5
 odiUsername=$6
 odiUserPwd=$7
+key=$8
 
 validateInputs
 generateConnectionProperties
 getOdiObjectsDirectory
 
 echo "Importing objects from directory ${odiObjectsDirectory}..."
-result=$(/Users/matteofoiadelli/Documents/Development/OdiUtils/src/import-objects.sh -c ${connectionPropertiesFile} ${odiObjectsDirectory}; echo $?)
+if [[ -z ${key} ]]
+then 
+	result=$(import-objects.sh -c ${connectionPropertiesFile} ${odiObjectsDirectory}; echo $?)
+else
+	result=$(import-objects.sh -c ${connectionPropertiesFile} -k ${key} ${odiObjectsDirectory}; echo $?)
+fi
 [[ ${result} -eq 0 ]] && echo " Done!" || echo "::error::ERROR: Import process failed. Check the logs above for further details."
 
 echo "Cleaning up..."
